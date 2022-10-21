9846da701d7d84fcd092e24a41bd08970d939ad2
feixiao <feixiao2020@sina.com>
2022-10-21 15:49:13 +0800

    存在没有显示函数名的问题 https://stackoverflow.com/questions/72165484/gperftools-not-showing-function-names

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,11 @@
 ### gperftools 快速入门
 
-
+#### gperftools
+```shell
+arch -x86_64 /usr/local/bin/brew  install gperftools
+ibrew install ps2pdf
+```
 
 #### 参考资料
 + [《google heap profiler 使用》](https://www.twblogs.net/a/5b8354f72b71776c51e29838/?lang=zh-cn)
++ [heap-checker](https://gperftools.github.io/gperftools/heap_checker.html)
--- memory/main.cpp
+++ memory/main.cpp
@@ -5,18 +5,31 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <gperftools/heap-profiler.h>
 
 void malloc(std::vector<uint8_t*>& array, uint16_t length);
 void  free(std::vector<uint8_t*>& array);
 
+
+// export LD_PRELOAD=/usr/local/Cellar/gperftools/2.9.1_1/lib/libtcmalloc.dylib
+// env HEAPPROFILE=/Users/frank/workspace/github/gperftools_quick_start/mem HEAPCHECK=normal ./cmake-build-debug/memory/memory
+//  pprof --pdf  ./cmake-build-debug/memory/memory mem.0001.heap > 1.pdf
+// pprof --text  ./cmake-build-debug/memory/memory mem.0001.heap > 1.txt
 int main() {
     printf("hello gperftools\n");
 
+    HeapProfilerStart("memory");
     std::vector<uint8_t*> array;
 
-    malloc(array, 16);
-    free(array);
+    for (int i = 0; i < 500; ++i) {
+        printf("****** %d ******\n", i);
+        malloc(array, 16);
+        free(array);
+        array.clear();
+    }
+    HeapProfilerDump("END");
 
+    HeapProfilerStop();
     return 0;
 }
 
