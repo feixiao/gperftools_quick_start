26e6fb0e58a9c179f5449f095f67ea08ee73679f
feixiao <feixiao2020@sina.com>
2022-10-21 14:21:50 +0800

    准备测试demo

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+### gperftools 快速入门
+
+
+
+#### 参考资料
++ [《google heap profiler 使用》](https://www.twblogs.net/a/5b8354f72b71776c51e29838/?lang=zh-cn)
--- memory/main.cpp
+++ memory/main.cpp
@@ -3,8 +3,47 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <random>
+
+void malloc(std::vector<uint8_t*>& array, uint16_t length);
+void  free(std::vector<uint8_t*>& array);
 
 int main() {
     printf("hello gperftools\n");
+
+    std::vector<uint8_t*> array;
+
+    malloc(array, 16);
+    free(array);
+
     return 0;
 }
+
+void malloc(std::vector<uint8_t*>& array, uint16_t length) {
+    for (int i = 0; i < length; ++i) {
+        std::default_random_engine e;
+        std::uniform_int_distribution<unsigned> u(2, 256);
+        uint8_t* p = new uint8_t[u(e)];
+        array.push_back(p);
+    }
+}
+
+void  free(std::vector<uint8_t*>& array) {
+    std::default_random_engine e;
+    std::uniform_int_distribution<unsigned> u(1, array.size());
+
+    int count = u(e);
+
+    for (int i = 0; i < count; ++i) {
+        std::default_random_engine e1;
+        std::uniform_int_distribution<unsigned> u1(0, array.size());
+        int index = u1(e1);
+
+        if(array[index] != nullptr) {
+            delete [] array[index];
+            array[index] = nullptr;
+        }
+    }
+}
+
